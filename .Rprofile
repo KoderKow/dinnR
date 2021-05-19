3b07763201bf1e3a1b6b1b1c1ee4701927ca0d76
KoderKow <koderkow@gmail.com>
2021-05-19 00:13:15 -0500

    testing auto deploy

--- .Rprofile
+++ .Rprofile
@@ -1,4 +1,7 @@
-source("renv/activate.R")
+if (file.exists("renv/activate.R")) {
+  source("renv/activate.R")
+}
+
 ## Run personal .Rprofile
 if (file.exists("~/.Rprofile")) {
   source("~/.Rprofile")
--- deploy.R
+++ deploy.R
@@ -7,16 +7,17 @@ error_on_missing_name <- function(name){
   }
   gsub("\"", '',var)
 }
+
 # Authenticate
 setAccountInfo(
   name = error_on_missing_name("SHINY_ACC_NAME"),
   token = error_on_missing_name("TOKEN"),
   secret = error_on_missing_name("SECRET")
 )
+
 # Deploy the application.
 deployApp(
-  appFiles = c("app.R"),
   appName = error_on_missing_name("MASTERNAME"),
-  appTitle = "shinyapplication"
+  appTitle = "dinnR"
 )
 
